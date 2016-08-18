b1ecb1cdc4898195febabeba84b77002d9f74cd8
/Fox --develop <paul.liverman.iii@gmail.com>
2016-08-18 16:27:38 -0700

    Create hex_dump.lua

---
+++ hex_dump.lua
@@ -0,0 +1,29 @@
+-- this was copied from https://gist.githubusercontent.com/Elemecca/6361899/raw/3c0e08285f452eb65268946f1695b8eb1117ed5b/hex_dump.lua
+-- this is temporary at the moment for testing if it even works for my use case (githook)
+
+function hex_dump (str)
+    local len = string.len( str )
+    local dump = ""
+    local hex = ""
+    local asc = ""
+    
+    for i = 1, len do
+        if 1 == i % 8 then
+            dump = dump .. hex .. asc .. "\n"
+            hex = string.format( "%04x: ", i - 1 )
+            asc = ""
+        end
+        
+        local ord = string.byte( str, i )
+        hex = hex .. string.format( "%02x ", ord )
+        if ord >= 32 and ord <= 126 then
+            asc = asc .. string.char( ord )
+        else
+            asc = asc .. "."
+        end
+    end
+
+    
+    return dump .. hex
+            .. string.rep( "   ", 8 - len % 8 ) .. asc
+end
